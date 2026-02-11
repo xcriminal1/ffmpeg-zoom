8535c3ae8051b937e5834789372cd54e17a42c1a
Aman Singh <aaryanamanaa19@gmail.com>
2026-02-11 18:54:06 +0530

    8th attempt

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 const N8N_WEBHOOK_URL = process.env.N8N_WEBHOOK_URL;
 
 // -------------------- SESSION STATE --------------------
